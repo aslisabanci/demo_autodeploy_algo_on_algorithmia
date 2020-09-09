dd31d2b0dee1d07d648a7b5364eacb8afbe6f950
aslisabanci <aslisabanci@gmail.com>
2020-09-09 11:50:20 -0700

    Simplified to only config updates

--- .github/actions/algorithmia_ci_modeldeployment/entrypoint.py
+++ .github/actions/algorithmia_ci_modeldeployment/entrypoint.py
@@ -7,12 +7,19 @@ from typing import AnyStr
 from src import algorithmia_utils, notebook_utils
 
 
-def load_algo_model_config(base_path, config_rel_path="model_config.json"):
+def update_algo_model_config(
+    base_path, commit_hash, config_rel_path="model_config.json"
+):
     full_path = "{}/{}".format(base_path, config_rel_path)
-    with open(full_path) as json_file:
+    with open(full_path, "r+") as json_file:
         config = json.load(json_file)
-        print(config.keys(), config.values())
-        return config
+        print("old hash", config["model_origin_commit_hash"])
+        config["model_origin_commit_hash"] = commit_hash
+        json.dump(config, json_file)
+
+    with open(full_path, "r+") as json_file:
+        new_config = json.load(json_file)
+        print("new hash", new_config["model_origin_commit_hash"])
 
 
 if __name__ == "__main__":
@@ -38,19 +45,18 @@ if __name__ == "__main__":
 
     if os.path.exists(workspace):
         algo_dir = "{}/{}".format(workspace, algo_name)
-        config = load_algo_model_config(algo_dir)
-        print(config)
+        update_algo_model_config(algo_dir, current_commit_hash)
 
-        workspace_notebook_path = "{}/{}".format(workspace, notebook_path)
-        print("workspace notebook path:", workspace_notebook_path)
-        notebook_utils.run_notebook(
-            notebook_path=workspace_notebook_path, execution_path=workspace
-        )
+        # workspace_notebook_path = "{}/{}".format(workspace, notebook_path)
+        # print("workspace notebook path:", workspace_notebook_path)
+        # notebook_utils.run_notebook(
+        #     notebook_path=workspace_notebook_path, execution_path=workspace
+        # )
 
-        model_full_path = "{}/{}".format(workspace, model_rel_path)
-        algorithmia_utils.upload_model(
-            algorithmia_api_key, model_full_path, upload_path
-        )
+        # model_full_path = "{}/{}".format(workspace, model_rel_path)
+        # algorithmia_utils.upload_model(
+        #     algorithmia_api_key, model_full_path, upload_path
+        # )
     else:
         raise Exception(
             "actions/checkout on the local repo must be run before this action can be completed"
