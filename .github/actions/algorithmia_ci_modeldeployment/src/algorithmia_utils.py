a36b015a1002e0735bc59083266dc99a91e5a37f
aslisabanci <aslisabanci@gmail.com>
2020-09-10 16:03:57 -0700

    Refactored naming and encapsulation

--- .github/actions/algorithmia_ci_modeldeployment/src/algorithmia_utils.py
+++ .github/actions/algorithmia_ci_modeldeployment/src/algorithmia_utils.py
@@ -1,5 +1,7 @@
 import Algorithmia
 import os
+import json
+from datetime import datetime
 
 
 def upload_model(api_key, local_path, remote_path, commit_hash):
@@ -19,3 +21,31 @@ def upload_model(api_key, local_path, remote_path, commit_hash):
     # TODO: Act on the result object, have a return value
     print(result)
     return full_remote_path
+
+
+def update_algo_model_config(
+    base_path,
+    github_repo,
+    commit_hash,
+    model_filepath,
+    config_rel_path="model_config.json",
+):
+    full_path = "{}/{}".format(base_path, config_rel_path)
+    if os.path.exists(full_path):
+        with open(full_path, "r") as config_file:
+            config = json.load(config_file)
+            print("old hash", config["model_origin_commitHash"])
+
+        config["model_filePath"] = model_filepath
+        config["model_origin_commitHash"] = commit_hash
+        config["model_origin_repo"] = github_repo
+        config["model_uploadedAt_UTC"] = datetime.utcnow().strftime(
+            "%Y-%m-%d %H:%M:%S.%f"
+        )
+
+        with open(full_path, "w") as new_config_file:
+            print("new hash", config["model_origin_commitHash"])
+            json.dump(config, new_config_file)
+    else:
+        # TODO: If model_config.json doesn't exist, create it from scratch
+        pass
