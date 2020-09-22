30de28eebdac51bcf6c840a2a86914fdf8e217c4
aslisabanci <aslisabanci@gmail.com>
2020-09-21 22:05:25 -0700

    Testing with a new algo

--- pull_algorithmia.sh
+++ pull_algorithmia.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone --depth=1 https://git.algorithmia.com/git/asli/demo_gh.git
-rm -rf demo_gh/.git
+git clone --depth=1 https://git.algorithmia.com/git/asli/demo_algo.git
+rm -rf demo_algo/.git
