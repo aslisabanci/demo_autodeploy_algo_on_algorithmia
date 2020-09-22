7de39c17f091f737265b25c34ef44d0315695a5d
aslisabanci <aslisabanci@gmail.com>
2020-09-21 21:23:15 -0700

    Connecting to a new algo

--- demo_jameskenny/src/demo_jameskenny_test.py
+++
@@ -1,4 +0,0 @@
-from . import demo_jameskenny
-
-def test_demo_jameskenny():
-    assert demo_jameskenny.apply("Jane") == "hello Jane"
--- pull_algorithmia.sh
+++ pull_algorithmia.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ALGO_NAME=demo_jameskenny
-ALGO_REPO=https://git.algorithmia.com/git/asli/demo_jameskenny.git
+ALGO_NAME=demo_gh
+ALGO_REPO=https://git.algorithmia.com/git/asli/demo_gh.git
 
 git clone --depth=1 $ALGO_REPO
 cd $ALGO_NAME
